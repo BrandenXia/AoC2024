1e46118582e026701a2f2fb56555000a3f2437db
BrandenXia <xxtbranden@outlook.com>
2024-12-13 16:02:20 -0800

    Day 13

---
+++ day13/first.jl
@@ -0,0 +1,25 @@
+#!/usr/bin/env julia
+
+using LinearAlgebra
+
+open("input.txt", "r") do f
+  input = read(f, String)
+  rx = r"Button A: X\+(\d+), Y\+(\d+)\nButton B: X\+(\d+), Y\+(\d+)\nPrize: X=(\d+), Y=(\d+)"
+  global machines = eachmatch(rx, input) .|> m -> map((
+    ax = m.captures[1],
+    ay = m.captures[2],
+    bx = m.captures[3],
+    by = m.captures[4],
+    px = m.captures[5],
+    py = m.captures[6]
+   )) do x parse(Int, x) // 1 end
+end
+costs = map(machines) do m
+  A = [m.ax m.bx; m.ay m.by]
+  b = [m.px; m.py]
+  x = inv(A) * b
+  if x[1] < 0 || x[2] < 0 return 0 end
+  if x[1] != float(x[1]) || x[2] != float(x[2]) return 0 end
+  return x[1] * 3 + x[2]
+end
+costs |> sum |> println
